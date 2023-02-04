668127b392302a994edcd640318075fc42341245
Sam O'Cain <samocain93@gmail.com>
2023-02-03 21:37:52 -0600

    index template example

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 const Sequelize = require('sequelize');
+
+// Import environment variables from the .env file
 require('dotenv').config();
 
+// Create a variable to store the database connection
 let sequelize;
 
+// Check if the JAWSDB_URL environment variable is set
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        port: 3306
-      }
-    );
-  }
-  
-  module.exports = sequelize;
+  // If JAWSDB_URL is set, use it to connect to the database
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // If JAWSDB_URL is not set, use the local MySQL database
+  sequelize = new Sequelize(
+    process.env.DB_NAME, // Database name
+    process.env.DB_USER, // Database username
+    process.env.DB_PASSWORD, // Database password
+    {
+      host: '127.0.0.1', // Database host
+      dialect: 'mysql', // Use the MySQL dialect
+      port: 3306 // Database port
+    }
+  );
+}
+
+// Export the database connection
+module.exports = sequelize;
+
   
